a4e1d03d5f94e911fefb54d69edf6757c247da09
rohit <rkg10072000@gmail.com>
2022-06-08 09:55:44 +0530

    implemented the error boundary

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,15 @@ import { Provider } from "react-redux";
 import Authguard from "../components/auth-guard/AuthGuard";
 import store from "../store";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ErrorBoundary from "../components/error-boundary/ErrorBoundary";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Authguard>
-        <Component {...pageProps} />
-      </Authguard>
+      <ErrorBoundary>
+        <Authguard>
+          <Component {...pageProps} />
+        </Authguard>
+      </ErrorBoundary>
     </Provider>
   );
 }
