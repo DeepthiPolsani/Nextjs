6b0b1cd85693ddb73ec957b6b316384d164c78b9
rohit <rkg10072000@gmail.com>
2022-06-15 12:16:00 +0530

    changed the signature of connecting to database

--- server/configs/next-connect.ts
+++ server/configs/next-connect.ts
@@ -1,17 +1,5 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import nextConnect from "next-connect";
-import { DataSource } from "typeorm";
-import connectDb from "./connect-db";
 
-export interface NextApiRequestExtended extends NextApiRequest {
-  myDataSource: DataSource;
-}
-
-const handler = nextConnect().use(
-  async (req: NextApiRequestExtended, res: NextApiResponse, _next) => {
-    req.myDataSource = await connectDb();
-    _next();
-  }
-);
+const handler = nextConnect();
 
 export default handler;
