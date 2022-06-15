bebd57e9666a56d415dc7c3ed0af962d64bfe527
rohit <rkg10072000@gmail.com>
2022-06-15 12:18:51 +0530

    deleted the user route

--- src/services/login.ts
+++ src/services/login.ts
@@ -1,10 +1,11 @@
 import apiFactory from "../utils/api-factory";
 
 export const validateUserLoginCredentials = async (credentials: any) => {
-  const response = await apiFactory().post("api/", {
-    path: "login",
+  const response = await apiFactory().post("api/auth/login", {
     username: credentials.username,
     password: credentials.password,
   });
+
+  console.log("printing", response.data);
   return response.data;
 };
