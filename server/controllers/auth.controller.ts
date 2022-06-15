155a416028a22c067561244a290fa72bd0f9ea1d
rohit <rkg10072000@gmail.com>
2022-06-15 14:35:26 +0530

    created register route

--- server/controllers/auth.controller.ts
+++ server/controllers/auth.controller.ts
@@ -14,4 +14,30 @@ const login = async (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ user });
 };
 
-export default { login };
+const register = async (req: NextApiRequest, res: NextApiResponse) => {
+  const {
+    username,
+    password,
+    firstName,
+    lastName,
+  }: {
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+  } = req.body;
+
+  if (!username || !password || !firstName || !lastName) {
+    throw new Error("please provide all details");
+  }
+
+  const user = await authService.regsiter({
+    username,
+    password,
+    firstName,
+    lastName,
+  });
+  res.status(201).json({ user });
+};
+
+export default { login, register };
--- server/services/auth.service.ts
+++ server/services/auth.service.ts
@@ -26,4 +26,36 @@ const login = async ({
   return user;
 };
 
-export default { login };
+const regsiter = async ({
+  username,
+  password,
+  firstName,
+  lastName,
+}: {
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+}) => {
+  const userRepo = await getRepo("User");
+  const userRegsitered = (await userRepo.findOne({
+    where: {
+      username,
+    },
+  })) as User;
+
+  if (userRegsitered) {
+    throw new Error(`user with ${username} already exists`);
+  }
+
+  const user = (await userRepo.create({
+    username,
+    password,
+    firstName,
+    lastName,
+  })) as User;
+  userRepo.save(user);
+  return user;
+};
+
+export default { login, regsiter };
